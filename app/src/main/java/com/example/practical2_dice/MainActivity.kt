817476481605bc1e213909f951dd6b8a673aec93
meilin0438 <chinmeilin0438@gmail.com>
2021-01-30 16:27:31 +0800

    30/1/2021 sum 2 dices

--- app/src/main/java/com/example/practical2_dice/MainActivity.kt
+++ app/src/main/java/com/example/practical2_dice/MainActivity.kt
@@ -19,9 +19,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        var x=0
+        var y=0
+        var total=0
         // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        x=diceRoll
 
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
@@ -41,6 +45,26 @@ class MainActivity : AppCompatActivity() {
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
+
+        //dice 2
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        y=diceRoll1
+        total=x+y
+        val resultTextView1: TextView = findViewById(R.id.textView_result)
+        resultTextView1.text = total.toString()
+        val diceImage1: ImageView = findViewById(R.id.imageView1)
+
+        val drawableResource1 = when (diceRoll1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage1.setImageResource(drawableResource1)
+        diceImage1.contentDescription = diceRoll1.toString()
     }
 
     class Dice(private val numSides: Int) {
